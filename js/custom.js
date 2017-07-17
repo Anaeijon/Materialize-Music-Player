844ed234813f8e7f724a9509b423d02b24a5380f
Anaeijon <david.doering@knopf-und-wolle.de>
2017-07-17 02:50:52 +0200

    tons of changes

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,19 @@
 $('document').ready(function() {
 
+	var musicApp = {};
+
 	//TODO: modify this array to sync with song list on backend
-	var songs = new Array();
-	$('.songPlay').each(function() {
-		$(this).parents('.collection-item');
-		songs.push($(this));
-	});
+	// musicApp.songs = new Array();
+	musicApp.songs = [];
 
 	//TODO: modify this Audio to sync with playing Audio on backend
-	var song = new Audio();
+	musicApp.currentSong = new Audio();
+
+	$('.songPlay').each(function() {
+		$(this).parents('.collection-item');
+		musicApp.songs.push($(this));
+	});
+
 
 	// Onclick action for song play buttons
 	$('.songPlay').on("click", function() {
@@ -43,18 +48,18 @@ $('document').ready(function() {
 
 			$("#play").text('pause_circle_outline');
 
-			song.src = 'musics/' + src;
-			song.volume = 1;
-			song.play();
+			musicApp.currentSong.src = 'musics/' + src;
+			musicApp.currentSong.volume = 1;
+			musicApp.currentSong.play();
 
-			song.addEventListener('loadedmetadata', function() {
+			musicApp.currentSong.addEventListener('loadedmetadata', function() {
 
 				var title = _this.attr('data-name');
 				$('.title').text(title);
 
-				$('#duration').attr('max', song.duration);
+				$('#duration').attr('max', musicApp.currentSong.duration);
 
-				var duration = song.duration;
+				var duration = musicApp.currentSong.duration;
 				$('.duration').text(formatSecondsAsTime(duration));
 
 			});
@@ -62,7 +67,7 @@ $('document').ready(function() {
 		} else if (_this.attr('state') == 'playing') {
 			$("#play").text('play_circle_outline');
 			_this.text('play_arrow');
-			song.pause();
+			musicApp.currentSong.pause();
 			_this.attr({
 				'state': 'stop'
 			});
@@ -70,92 +75,110 @@ $('document').ready(function() {
 
 	});
 
-	song.addEventListener('timeupdate', function() {
-		$(".current").text(formatSecondsAsTime(song.currentTime))
-		curtime = parseInt(song.currentTime, 10);
+
+	musicApp.currentSong.addEventListener('timeupdate', function() {
+		$(".current").text(formatSecondsAsTime(musicApp.currentSong.currentTime))
+		curtime = parseInt(musicApp.currentSong.currentTime, 10);
 		$("#duration").val(curtime);
 	});
+
 
 	$('#play').on('click', function() {
 		var _this = $(this);
 		if (_this.text() == 'play_circle_outline') {
-			song.play();
+			musicApp.currentSong.play();
 			_this.text('pause_circle_outline');
 		} else if (_this.text() == 'pause_circle_outline') {
-			song.pause();
+			musicApp.currentSong.pause();
 			_this.text('play_circle_outline');
 
 		}
 	});
+
+
 	var shuffle = 0;
 	$('#shuffle').on('click', function() {
-		if (shuffle == 0) {
+		if (shuffle === 0) {
 			$(this).css({
 				'color': '#ef5350'
 			});
 			shuffle = 1;
-		} else if (shuffle == 1) {
+		} else if (shuffle === 1) {
 			$(this).css({
 				'color': '#fff'
 			});
 			shuffle = 0;
 		}
 	});
+
+
 	var repeat = 0;
+
+
 	$("#repeat").on('click', function() {
-		if (repeat == 0) {
+		if (repeat === 0) {
 			$(this).text('repeat_one');
 			repeat = 1;
-		} else if (repeat == 1) {
+		} else if (repeat === 1) {
 			$(this).text('repeat');
 			repeat = 0;
 		}
 	});
+
+
 	$("#next").on('click', function() {
 		var _this = $(this);
-		if (shuffle == 1) {
-			var random = songs[Math.floor(songs.length * Math.random())];
+		if (shuffle === 1) {
+			var random = musicApp.songs[Math.floor(musicApp.songs.length * Math.random())];
 			random.trigger('click');
-		} else if (shuffle == 0) {
+		} else if (shuffle === 0) {
 			var item = $('.collection-item[status="playing"]').next('.collection-item');
 			item.find('.songPlay').trigger('click');
 
 		}
 	});
+
+
 	$("#prev").on('click', function() {
 		var _this = $(this);
-		if (shuffle == 1) {
-			var random = songs[Math.floor(songs.length * Math.random())];
+		if (shuffle === 1) {
+			var random = musicApp.songs[Math.floor(musicApp.songs.length * Math.random())];
 			random.trigger('click');
-		} else if (shuffle == 0) {
+		} else if (shuffle === 0) {
 			var item = $('.collection-item[status="playing"]').prev('.collection-item');
 			item.find('.songPlay').trigger('click');
 
 		}
 	});
-	song.addEventListener("ended", function() {
-		if (repeat == 1) {
-			song.currentTime = 0;
-			song.play();
-		} else if (repeat == 0) {
+	musicApp.currentSong.addEventListener("ended", function() {
+		if (repeat === 1) {
+			musicApp.currentSong.currentTime = 0;
+			musicApp.currentSong.play();
+		} else if (repeat === 0) {
 			$('#next').trigger('click');
 		}
 	});
+
+
 	var volume = 1;
+
+
 	$('#volume').on('click', function() {
-		if (volume == 1) {
+		if (volume === 1) {
 			$(this).text('volume_off');
-			song.volume = 0;
+			musicApp.currentSong.volume = 0;
 			volume = 0;
-		} else if (volume == 0) {
+		} else if (volume === 0) {
 			$(this).text('volume_up');
-			song.volume = 1;
+			musicApp.currentSong.volume = 1;
 			volume = 1;
 		}
 	});
+
+
 	$("#duration").on("change", function() {
-		song.currentTime = $(this).val();
-		$("#duration").attr("max", song.duration);
+		musicApp.currentSong.currentTime = $(this).val();
+		$("#duration").attr("max", musicApp.currentSong.duration);
 	});
 
 	function formatSecondsAsTime(secs) {
